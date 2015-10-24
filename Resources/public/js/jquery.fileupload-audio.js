056102fcf456b5a49d7889b74bc354b16950730e
andrey1s <andrey@4devs.pro>
2015-10-24 18:02:21 +0300

    update jquery.fileupload

--- Resources/public/js/jquery.fileupload-audio.js
+++ Resources/public/js/jquery.fileupload-audio.js
@@ -1,5 +1,5 @@
 /*
- * jQuery File Upload Audio Preview Plugin 1.0.3
+ * jQuery File Upload Audio Preview Plugin
  * https://github.com/blueimp/jQuery-File-Upload
  *
  * Copyright 2013, Sebastian Tschan
@@ -10,7 +10,7 @@
  */
 
 /* jshint nomen:false */
-/* global define, window, document */
+/* global define, require, window, document */
 
 (function (factory) {
     'use strict';
@@ -19,8 +19,14 @@
         define([
             'jquery',
             'load-image',
-            './jquery.fileupload-process.js'
+            './jquery.fileupload-process'
         ], factory);
+    } else if (typeof exports === 'object') {
+        // Node/CommonJS:
+        factory(
+            require('jquery'),
+            require('load-image')
+        );
     } else {
         // Browser globals:
         factory(
